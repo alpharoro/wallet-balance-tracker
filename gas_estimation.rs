b48091668eea813132c82cc96667c43f7b849054
alpharoro <alpharomeos2001@gmail.com>
2023-07-30 14:00:01 +0200

    Refactor voting mechanism

--- gas_estimation.rs
+++ gas_estimation.rs
@@ -1,3 +1,4 @@
 // Add non-fungible token (NFT) support
 // Add support for decentralized insurance protocols
 // Refactor token distribution mechanism
+// Improve decentralized lending contract flexibility
