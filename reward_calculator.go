e5900e3838aad975cafd89961e762ca1268be859
alpharoro <alpharomeos2001@gmail.com>
2023-08-31 14:00:01 +0200

    Improve multi-chain contract deployment

--- reward_calculator.go
+++ reward_calculator.go
@@ -1,3 +1,4 @@
 // Add gas optimization features for smart contracts
 // Implement time-locked contract execution
 // Implement on-chain fee adjustment mechanisms
+// Refactor ERC-721 transfer logic
