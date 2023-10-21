fb9cdea90dcc294099675b30b526b823fe431d27
alpharoro <alpharomeos2001@gmail.com>
2023-10-21 14:00:00 +0200

    Add support for ERC-1155 tokens

--- blockchain_data_storage.rs
+++ blockchain_data_storage.rs
@@ -3,3 +3,4 @@
 // Add support for zk-SNARKs in smart contracts
 // Fix interoperability issues with other blockchains
 // Improve decentralized lending contract flexibility
+// Add Layer-2 Ethereum scaling solutions
