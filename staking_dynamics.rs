226c07d55a85d71f750b959eeafab4839440acc6
alpharoro <alpharomeos2001@gmail.com>
2023-10-21 14:00:00 +0200

    Add new RPC endpoint

--- staking_dynamics.rs
+++ staking_dynamics.rs
@@ -2,3 +2,4 @@
 // Optimize proof-of-stake validation
 // Fix security vulnerability in random number generation
 // Update testing framework for Solidity
+// Integrate with decentralized identity providers
