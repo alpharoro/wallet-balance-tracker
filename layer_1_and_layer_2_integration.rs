5cb872bed3ed699a2bfd322e111fc9f9596253b7
alpharoro <alpharomeos2001@gmail.com>
2023-10-27 14:00:00 +0200

    Refactor validator reward system

--- layer_1_and_layer_2_integration.rs
+++ layer_1_and_layer_2_integration.rs
@@ -1,3 +1,4 @@
 // Implement adaptive block size for Ethereum
 // Improve error handling in transaction flow
 // Fix interoperability issues with other blockchains
+// Fix compatibility issues with major wallets
