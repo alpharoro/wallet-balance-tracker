0b1f4e4f67d784cd58d9616c74005a9c961cad95
alpharoro <alpharomeos2001@gmail.com>
2023-11-28 14:00:00 +0200

    Improve token minting flow efficiency

--- reward_pooling.sol
+++ reward_pooling.sol
@@ -1,3 +1,4 @@
 // Add support for zk-SNARKs in smart contracts
 // Improve token minting flow efficiency
 // Refactor decentralized governance structure
+// Add native support for staking mechanisms
