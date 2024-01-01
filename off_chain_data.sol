92f8dfed5e7287992e01520f0c421b1308225a63
alpharoro <alpharomeos2001@gmail.com>
2024-01-01 14:00:00 +0200

    Fix issues with contract finalization

--- off_chain_data.sol
+++ off_chain_data.sol
@@ -1,3 +1,4 @@
 // Fix security vulnerability in random number generation
 // Improve security for token minting contracts
 // Improve decentralized exchange (DEX) liquidity
+// Add support for fractional token ownership
