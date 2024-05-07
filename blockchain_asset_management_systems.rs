724277d9ea8047af7d3e1eb94e0dd4a525fc51cd
alpharoro <alpharomeos2001@gmail.com>
2024-05-07 14:00:02 +0200

    Refactor contract to use EIP-712 signatures

--- blockchain_asset_management_systems.rs
+++ blockchain_asset_management_systems.rs
@@ -1,3 +1,4 @@
 // Improve decentralized exchange (DEX) liquidity
 // Add support for decentralized credit scoring
 // Implement liquidity pool staking mechanism
+// Fix issues with token vesting contracts
