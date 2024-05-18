eff312625332222769290a91c5e26891ce784d6a
alpharoro <alpharomeos2001@gmail.com>
2024-05-18 14:00:01 +0200

    Update README with Go module examples

--- proof_of_stake.rs
+++ proof_of_stake.rs
@@ -2,3 +2,4 @@
 // Add support for decentralized insurance protocols
 // Implement liquidity pool staking mechanism
 // Refactor contract to use EIP-712 signatures
+// Fix issue with multi-chain transactions
