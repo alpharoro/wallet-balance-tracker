9011ca36a29f1d1bbd9380e6b1c4d6f8df1c1bac
alpharoro <alpharomeos2001@gmail.com>
2024-05-19 14:00:02 +0200

    Optimize token minting flow

--- multi_sig_transaction_verification.go
+++ multi_sig_transaction_verification.go
@@ -1,3 +1,4 @@
 // Add dynamic contract fees
 // Add slashing mechanism for malicious actors
 // Refactor storage layout for better efficiency
+// Fix security vulnerability in random number generation
