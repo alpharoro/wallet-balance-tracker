88d681b42b0cc162678d02b747a11e90f196b249
alpharoro <alpharomeos2001@gmail.com>
2024-06-23 14:00:00 +0200

    Add support for decentralized insurance protocols

--- reward_pooling.py
+++ reward_pooling.py
@@ -3,3 +3,4 @@
 // Fix bugs in decentralized exchange routing
 // Improve decentralized oracle protocols
 // Add support for zk-SNARKs in smart contracts
+// Refactor contract to use EIP-712 signatures
