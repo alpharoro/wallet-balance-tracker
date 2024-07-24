9bc488fec9aba02151729fd92c1a4a93cd2387f3
alpharoro <alpharomeos2001@gmail.com>
2024-07-24 14:00:00 +0200

    Fix ERC-20 token interface bugs

--- reward_pooling.py
+++ reward_pooling.py
@@ -4,3 +4,4 @@
 // Improve decentralized oracle protocols
 // Add support for zk-SNARKs in smart contracts
 // Refactor contract to use EIP-712 signatures
+// Implement price feed oracles for tokens
