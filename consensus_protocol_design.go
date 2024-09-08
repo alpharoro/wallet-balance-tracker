27e4094466f9f9a0d143782bbe82a9844199f2e8
alpharoro <alpharomeos2001@gmail.com>
2024-09-08 14:00:00 +0200

    Add support for multiple consensus mechanisms

--- consensus_protocol_design.go
+++ consensus_protocol_design.go
@@ -2,3 +2,4 @@
 // Integrate decentralized oracles for data feeds
 // Improve contract state management
 // Add support for multiple consensus mechanisms
+// Integrate with decentralized prediction markets
