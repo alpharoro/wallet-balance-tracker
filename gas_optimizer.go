92ca23016cb395eaf03712b799d827a64526c1db
alpharoro <alpharomeos2001@gmail.com>
2024-10-18 14:00:00 +0200

    Refactor contract state machine design

--- gas_optimizer.go
+++ gas_optimizer.go
@@ -2,3 +2,4 @@
 // Add governance proposal expiration feature
 // Integrate Chainlink VRF for randomness
 // Add governance proposal expiration feature
+// Refactor contract to use EIP-712 signatures
