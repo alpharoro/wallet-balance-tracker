53b3bba6ddd5004447d58e6b1d3011b84182944b
alpharoro <alpharomeos2001@gmail.com>
2024-10-31 14:00:00 +0200

    Fix issue with multi-chain transactions

--- protocol_upgrading_mechanisms.sol
+++ protocol_upgrading_mechanisms.sol
@@ -2,3 +2,4 @@
 // Add slashing mechanism for malicious actors
 // Enhance cryptographic primitives in contracts
 // Fix issues in multi-token smart contracts
+// Add governance proposal expiration feature
